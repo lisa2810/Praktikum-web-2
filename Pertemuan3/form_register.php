082ac6d72025dbc5a2c77f2c8aad5ac900ca6c71
lisa2810 <monalisaharahap563@gmail.com>
2023-03-14 16:03:20 +0700

    Tugas Pertemuan 3

--- Pertemuan3/form_register.php
+++ Pertemuan3/form_register.php
@@ -1,24 +1,24 @@
 <?php
 // array majors
 $majors = [
-    'SI' => 'Sistem Informasi',
-    'TI' => 'Teknik Informatika',
+    'SI' => 'Sistem Informasi', 
+    'TI' => 'Tehnik Informatika', 
     'BD' => 'Bisnis Digital'
 ];
 
-// array skills
+//array skills
 $skills = [
     'HTML' => 10,
     'CSS' => 10,
     'JavaScript' => 20,
-    'RWD Booststrap' => 20,
+    'RWD Bootstrap' => 20,
     'PHP' => 30,
     'Python' => 30,
     'Java' => 50
 ];
 
 // array domisili
-$domisiles = ['Jakarta','Depok','Bogor','Tanggerang','Bekasi','Bandung'];
+$domisiles = ['Jakarta', 'Depok', 'Bogor', 'Tanggerang', 'Bekasi', 'Lainnya'];
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +27,7 @@ $domisiles = ['Jakarta','Depok','Bogor','Tanggerang','Bekasi','Bandung'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Register</title>
+    <title>Form Registrasi</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -35,77 +35,83 @@ $domisiles = ['Jakarta','Depok','Bogor','Tanggerang','Bekasi','Bandung'];
     <div class="container mt-5">
         <h2>Form Registrasi IT Club</h2>
         <hr>
-
-            <form method="POST" action="proses_register.php">
+        <form method="POST" action="proses_register.php">
             <div class="form-group row">
-                    <label for="nim" class="col-4 col-form-label">NIM</label> 
-                    <div class="col-8">
-                        <input id="nim" name="nim" placeholder="Masukkan NIM" type="text" class="form-control" required="required">
+                <label for="nim" class="col-4 col-form-label">NIM</label> 
+                <div class="col-8">
+                    <input id="nim" name="nim" placeholder="Masukkan NIM" type="text" class="form-control" required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nama" class="col-4 col-form-label">Nama</label> 
+                <div class="col-8">
+                    <input id="nama" name="nama" placeholder="Masukkan Nama" type="text" class="form-control" required="required">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-4">Jenis Kelamin</label> 
+                <div class="col-8">
+                    <div class="custom-control custom-radio custom-control-inline">
+                        <input name="gender" id="gender_0" type="radio" class="custom-control-input" value="Laki-laki" required="required"> 
+                        <label for="gender_0" class="custom-control-label">Laki-laki</label>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline">
+                        <input name="gender" id="gender_1" type="radio" class="custom-control-input" value="Perempuan" required="required"> 
+                        <label for="gender_1" class="custom-control-label">Perempuan</label>
                     </div>
                 </div>
-        <div class="form-group row">
-            <label for="nama" class="col-4 col-form-label">Nama</label> 
-            <div class="col-8">
-            <input id="nama" name="nama" placeholder="Masukkan Nama" type="text" class="form-control" required="required">
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-4">Jenis Kelamin</label> 
-            <div class="col-8">
-            <div class="custom-control custom-radio custom-control-inline">
-                <input name="gender" id="gender_0" type="radio" class="custom-control-input" value="Laki-laki" required="required"> 
-                <label for="gender_0" class="custom-control-label">Laki-laki</label>
+            <div class="form-group row">
+                <label for="prodi" class="col-4 col-form-label">Program Studi</label> 
+                <div class="col-8">
+                    <select id="prodi" name="prodi" class="custom-select" required="required">
+                        <?php
+                        foreach($majors as $key => $value){
+                            echo '<option value="'. $value .'">'. $value .'</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
-            <div class="custom-control custom-radio custom-control-inline">
-                <input name="gender" id="gender_1" type="radio" class="custom-control-input" value="Perempuan" required="required"> 
-                <label for="gender_1" class="custom-control-label">Perempuan</label>
+            <div class="form-group row">
+                <label class="col-4">Skill Programming</label> 
+                <div class="col-8">
+                    <?php foreach($skills as $key => $value){ ?>
+                        <div class="custom-control custom-checkbox custom-control-inline">
+                            <input name="skill[]" id="<?= $key ?>" type="checkbox" class="custom-control-input" value="<?= $key ?>"> 
+                            <label for="<?= $key ?>" class="custom-control-label"><?= $key ?></label>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
+            <div class="form-group row">
+                <label for="domisili" class="col-4 col-form-label">Tempat Domisili</label> 
+                <div class="col-8">
+                    <select id="domisili" name="domisili" class="custom-select" required="required">
+                        <?php foreach($domisiles as $domisile): ?>
+                            <option value="<?= $domisile ?>"><?= $domisile ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="prodi" class="col-4 col-form-label">Program studi</label> 
-            <div class="col-8">
-            <select id="prodi" name="prodi" class="custom-select" required="required">
-                    <?php
-                    foreach($majors as $key => $value){
-                        echo '<option value="'. $value .'">' . $value .'</option>';
-                    }
-                    ?>
-            </select>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-4">Skill Programming</label> 
-            <div class="col-8">
-                <?php foreach($skills as $key => $value){ ?>
-                    <div class="custom-control custom-checkbox custom-control-inline">
-                        <input name="skill[]" id="<?= $key ?>" type="checkbox" class="custom-control-input" value="<?= $key ?>"> 
-                        <label for="<?= $key ?>" class="custom-control-label"><?= $key ?></label>
+            <div class="form-group row">
+                <label for="email" class="col-4 col-form-label">Email</label> 
+                <div class="col-8">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                        </div> 
+                        <input id="email" name="email" placeholder="Masukkan Email" type="text" required="required" class="form-control">
                     </div>
-                <?php } ?>
+                </div>
+            </div> 
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="domisili" class="col-4 col-form-label">Tempat Domisili</label> 
-            <div class="col-8">
-            <select id="domisili" name="domisili" class="custom-select" required="required">
-                <?php foreach($domisiles as $domisile): ?>
-                    <option value="<?= $domisile ?>"><?= $domisile ?></option>
-                    <?php endforeach ?>
-            </select>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label for="email" class="col-4 col-form-label">Email</label> 
-            <div class="col-8">
-            <input id="email" name="email" placeholder="Masukkan Email" type="text" class="form-control" required="required">
-            </div>
-        </div> 
-        <div class="form-group row">
-            <div class="offset-4 col-8">
-            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
         </form>
     </div>
 </body>
--- Pertemuan3/proses_register.php
+++ Pertemuan3/proses_register.php
@@ -1,22 +1,66 @@
 <?php
-    if(isset($_POST['submit'])) {
-    $nim = $_POST['nim'];
-    $nama = $_POST['nama'];
-    $gender = $_POST['gender'];
-    $prodi = $_POST['prodi'];
-    $skills = $_POST['skill'];
-    $domisili = $_POST['domisili'];
-    $email = $_POST['email'];
+if(isset($_POST['submit'])){
+    $nim= $_POST['nim'];
+    $nama= $_POST['nama'];
+    $gender= $_POST['gender'];
+    $prodi= $_POST['prodi'];
 
-    echo "NIM: $nim";
-    echo "<br> Nama: $nama";
-    echo "<br> Jenis Kelamin: $gender";
-    echo "<br> Program Studi: $prodi";
-    echo "<br> Skills Programing: ";
-
-    foreach($skills as $skills) {
-        echo $skills . ", ";
+    if (!empty($_POST['skill'])){
+        $skills= $_POST['skill'];
     }
-    echo "<br> Domisili: $domisili";
-    echo "<br> Email: $email";
+    $domisili= $_POST['domisili'];
+    $email= $_POST['email'];
+
+    echo"NIM: $nim";
+    echo"<br> Nama: $nama";
+    echo"<br> Jenis Kelamin: $gender"; 
+    echo"<br> Program Studi: $prodi";
+    echo"<br> Skill Programming: ";
+
+    $listNilai = [];
+
+    if (!empty($_POST['skill'])) {
+        foreach($skills as $skill){
+            if($skill == 'HTML'){
+                array_push($listNilai,10);
+            }elseif($skill == 'CSS') {
+                array_push($listNilai,10);
+            }elseif($skill == 'JavaScript') {
+                array_push($listNilai,20);
+            }elseif($skill == 'RWD Bootstrap') {
+                array_push($listNilai,20);
+            }elseif($skill == 'PHP') {
+                array_push($listNilai,30);
+            }elseif($skill == 'Python') {
+                array_push($listNilai,30);
+            }elseif($skill == 'Java') {
+                array_push($listNilai,50);
+            }
+
+            echo $skill. ", ";
+        }
+    }
+
+    $nilai = array_sum($listNilai);
+
+    echo"<br> Domisili: $domisili";
+    echo"<br> Email: $email";
+    echo "<br> Total Nilai: $nilai";
+
+    //Tugas Praktikum 3
+
+    if ($nilai == 0) {
+        echo "<br> Predikat Tidak Memadai";
+    }elseif ($nilai > 0 AND $nilai <= 40) {
+        echo "<br> Predikat : Kurang"; 
+    }elseif ($nilai > 40 AND $nilai <= 60) {
+        echo "<br> Predikat : Cukup";
+    }elseif ($nilai > 60 AND $nilai <= 100) {
+        echo "<br> Predikat : Baik";
+    }elseif ($nilai > 100 AND $nilai <= 170) {
+        echo "<br> Predikat : Sangat Baik";
+    }else {
+        echo "<br> Skor Tidak Valid";
+    }
 }
+?>
